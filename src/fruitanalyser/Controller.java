800e60631f5ba2735a5d6a2d4df4cc69d481e510
Gowriswarup Kailas Perumal <kgswarup19@gmail.com>
2022-05-01 12:48:37 +0100

    upload fxml basic creation

---
+++ src/fruitanalyser/Controller.java
@@ -0,0 +1,14 @@
+package fruitanalyser;
+
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+
+
+
+}
